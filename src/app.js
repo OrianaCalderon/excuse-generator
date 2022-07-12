3a652c2f76d51e9ce71d083d24a99c64de01ed63
oscq12 <orianasofia86@gmail.com>
2022-07-12 22:13:15 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird", "my cat"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class.",
+    "right in time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was running."
+  ];
+
+  let who_excusa = Math.floor(Math.random() * who.length);
+  let what_excusa = Math.floor(Math.random() * what.length);
+  let when_excusa = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[who_excusa] + " " + what[what_excusa] + " " + when[when_excusa];
 };
